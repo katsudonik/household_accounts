0441b1d7e00646526eb5c321e193fbdfab7ef294
root <root@localhost.localdomain>
2018-06-11 00:56:33 +0900

    auto update old record's end_date

--- app/Model/PurchaseSchedule.php
+++ app/Model/PurchaseSchedule.php
@@ -51,4 +51,35 @@ class PurchaseSchedule extends AppModel {
 			'order' => ''
 		)
 	);
+
+	//TODO save: if exists same item & end_date is null,
+
+	public function save($data = null, $validate = true, $fieldList = []){
+	    $result = parent::save($data);
+	    if(!$result){
+	        return false;
+	    }
+
+	    if(empty($data['PurchaseSchedule']['target_start_date']) && empty($data['PurchaseSchedule']['target_end_date']) && empty($record)){
+	        return $result;
+	    }
+
+	    parent::create();
+	    $record = $this->find('first', [
+	        'conditions' => [
+	            'NOT' => ['PurchaseSchedule.id' => $this->getLastInsertID()],
+	            'target_date IS NULL',
+	            'target_end_date IS NULL',
+	            'item_id' => $data['PurchaseSchedule']['item_id']
+	        ],
+	    ]);
+
+	    if(empty($record)){
+	        return true;
+	    }
+
+        $record['PurchaseSchedule']['target_end_date'] = date('Y-m-d', strtotime("{$data['PurchaseSchedule']['target_start_date']} -1 day"));
+        $this->set($record);
+        return parent::save($record);
+	}
 }
